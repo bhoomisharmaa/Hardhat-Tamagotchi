a63cb06b69eb5510256496f4591621fb94d5fd86
bhoomisharmaa <bhoomisharma404@gmail.com>
2025-09-10 16:36:19 +0530

    we dont use process.env here

--- frontend/src/utils/wagmiConfig.ts
+++ frontend/src/utils/wagmiConfig.ts
@@ -4,10 +4,10 @@ import { http } from "wagmi";
 
 export const config = getDefaultConfig({
   appName: "Tamagotchi",
-  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID ?? "",
+  projectId: import.meta.env.VITE_WALLETCONNECT_PROJECT_ID ?? "",
   chains: [sepolia, hardhat],
   transports: {
-    [sepolia.id]: http(process.env.SEPOLIA_RPC_URL),
+    [sepolia.id]: http(import.meta.env.VITE_SEPOLIA_RPC_URL),
     [hardhat.id]: http("http://127.0.0.1:8545/"),
   },
   ssr: true,
