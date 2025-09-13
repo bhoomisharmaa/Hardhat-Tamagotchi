ecfe3c5157c377b500b41c42cef87feb125df1cd
bhoomisharmaa <bhoomisharma404@gmail.com>
2025-09-13 11:15:49 +0530

    react router dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,15 @@
 import "./index.css";
 import HomePage from "./components/homePage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  return <HomePage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
