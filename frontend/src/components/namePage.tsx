945931947ced3cd750b3ebcc2d640596e8363e91
bhoomisharmaa <bhoomisharma404@gmail.com>
2025-09-19 11:01:45 +0530

    i forgor

--- frontend/src/components/namePage.tsx
+++ frontend/src/components/namePage.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useAccount, useBalance, useWriteContract } from "wagmi";
 import type { Abi, Address } from "viem";
 import { waitForTransactionReceipt } from "wagmi/actions";
+import LoadingPage from "./loadingPage";
 
 type ContractConfig = {
   address: Address;
@@ -12,6 +13,7 @@ interface NamePageProps {
   contractConfig?: ContractConfig;
   address?: Address;
   config: any;
+  isLoading: boolean;
   setIsLoading: (loading: boolean) => void;
   chainId: 11155111 | 31337;
 }
@@ -20,6 +22,7 @@ export default function NamePage({
   contractConfig,
   address,
   config,
+  isLoading,
   setIsLoading,
   chainId,
 }: NamePageProps) {
@@ -42,10 +45,8 @@ export default function NamePage({
       });
       setIsLoading(true);
       const receipt = await waitForTransactionReceipt(config, { hash: txn });
-      console.log("Receipt:", receipt);
     } catch (error) {
       console.log(error);
-    } finally {
       setIsLoading(false);
     }
   };
@@ -60,42 +61,46 @@ export default function NamePage({
 
   return (
     <div className="h-screen w-screen bg-(--color-alice-blue) flex items-center justify-center font-tiny5">
-      <div className="flex flex-col items-start sm:gap-8 gap-1">
-        <p className="md:text-7xl sm:text-5xl text-3xl">TAMAGOTCHI</p>
-        <div className="h-max w-max md:p-6 sm:p-4 p-3 sm:border-3 border-2">
-          <div className="h-max w-max sm:border-3 border-2 border-dashed md:px-10 md:py-7 px-6 py-4 flex flex-col sm:gap-6 gap-1 items-center font-pressStart">
-            <div className="h-max w-full flex items-center justify-between mb md:text-sm xs:text-[8px] text-[6px]">
-              <p>{`Game balance: ${balance}`}</p>
-              <p>
-                {account.address?.slice(0, 6) +
-                  "....." +
-                  account.address?.slice(38)}
+      {isLoading ? (
+        <LoadingPage />
+      ) : (
+        <div className="flex flex-col items-start sm:gap-8 gap-1">
+          <p className="md:text-7xl sm:text-5xl text-3xl">TAMAGOTCHI</p>
+          <div className="h-max w-max md:p-6 sm:p-4 p-3 sm:border-3 border-2">
+            <div className="h-max w-max sm:border-3 border-2 border-dashed md:px-10 md:py-7 px-6 py-4 flex flex-col sm:gap-6 gap-1 items-center font-pressStart">
+              <div className="h-max w-full flex items-center justify-between mb md:text-sm xs:text-[8px] text-[6px]">
+                <p>{`Game balance: ${balance}`}</p>
+                <p>
+                  {account.address?.slice(0, 6) +
+                    "....." +
+                    account.address?.slice(38)}
+                </p>
+              </div>
+              <p className="md:text-2xl sm:text-xl xs:text-base xxs:text-[12px] text-[10px]">
+                Adopt your tamagotchi
               </p>
-            </div>
-            <p className="md:text-2xl sm:text-xl xs:text-base xxs:text-[12px] text-[10px]">
-              Adopt your tamagotchi
-            </p>
 
-            <form
-              onSubmit={handleAdoption}
-              className="w-full flex flex-col items-center"
-            >
-              <input
-                name="name"
-                className="w-full bg-white sm:py-3 sm:px-4 sm:text-base text-[10px] px-2 py-1 rounded-lg"
-                placeholder="Enter a name"
-                required
-              />
-              <button
-                type="submit"
-                className="mt-3 bg-blue-grey sm:px-5 sm:py-3 px-2 py-1 sm:border-2 border-1 sm:rounded-2xl rounded-md md:text-base sm:text-sm xs:text-xs text-[7px]"
+              <form
+                onSubmit={handleAdoption}
+                className="w-full flex flex-col items-center"
               >
-                Adopt
-              </button>
-            </form>
+                <input
+                  name="name"
+                  className="w-full bg-white sm:py-3 sm:px-4 sm:text-base text-[10px] px-2 py-1 rounded-lg"
+                  placeholder="Enter a name"
+                  required
+                />
+                <button
+                  type="submit"
+                  className="mt-3 bg-blue-grey sm:px-5 sm:py-3 px-2 py-1 sm:border-2 border-1 sm:rounded-2xl rounded-md md:text-base sm:text-sm xs:text-xs text-[7px]"
+                >
+                  Adopt
+                </button>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
