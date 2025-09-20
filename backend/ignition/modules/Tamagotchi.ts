5b1d327d6f71be6b1583b88c7c4e9f59edc6b444
bhoomisharmaa <bhoomisharma404@gmail.com>
2025-09-20 23:05:12 +0530

    uri

--- backend/ignition/modules/Tamagotchi.ts
+++ backend/ignition/modules/Tamagotchi.ts
@@ -14,22 +14,46 @@ export default buildModule("Tamagotchi", (m) => {
   const networkName = publicClient.chain.name.toLowerCase();
 
   // Image URI
-  const happyImageUri =
-    "https://orange-chemical-monkey-267.mypinata.cloud/ipfs/bafkreigsmxovabg3tszcj4kd4lyfoesjk5k4szr2yayqfb46endif67b5u";
-  const sadImageUri =
-    "https://orange-chemical-monkey-267.mypinata.cloud/ipfs/bafkreifmte3p7v2wr6xgocvmkdkbrazqzvw2dvwbqj27xxjbm566nn6d4a";
-  const neutralImageUri =
-    "https://orange-chemical-monkey-267.mypinata.cloud/ipfs/bafkreiewmm2zdidrabxyeqedfor4tu3utnmcultzbtx3uzmdezngjzbacm";
-  const hungryImageUri =
-    "https://orange-chemical-monkey-267.mypinata.cloud/ipfs/bafkreid3apinl4sygihf6j4yutstxckypf32vglozaidcpfr3jn7niphyq";
-  const boredImageUri =
-    "https://orange-chemical-monkey-267.mypinata.cloud/ipfs/bafkreibax2wix5dolg2ykfsskrloufpocdtgsyhxb7kemlfv2xitbgac2e";
-  const stinkyImageUri =
-    "https://orange-chemical-monkey-267.mypinata.cloud/ipfs/bafkreifa3my5pm34xjthggfecym5a54jfvc43qjhwaktjzznkegq7crez4";
-  const lethargicImageUri =
-    "https://orange-chemical-monkey-267.mypinata.cloud/ipfs/bafkreigdvmsvmhl4x5y4y2l5q3ncu7rtcsadvwimb2lq6w5svsatfo73ca";
-  const deadImageUri =
-    "https://orange-chemical-monkey-267.mypinata.cloud/ipfs/bafkreibw2562c7p7e5lgfb5ub6bz3yqumufxxtcptjzz4zra7nug7y7nku";
+  const happyImageUri = [
+    "https://orange-chemical-monkey-267.mypinata.cloud/ipfs/bafkreigsmxovabg3tszcj4kd4lyfoesjk5k4szr2yayqfb46endif67b5u",
+    "https://orange-chemical-monkey-267.mypinata.cloud/ipfs/bafkreicfbcxe6hj7tbmgh6rbiuipksg5k5bf2k44jfvvekfdka3lns4fiu",
+    "https://orange-chemical-monkey-267.mypinata.cloud/ipfs/bafkreigrpyhrr74ds7e7orsgg2kh4zalho2xsngm5dxr367cgs3mq6dwwy",
+  ];
+  const sadImageUri = [
+    "https://orange-chemical-monkey-267.mypinata.cloud/ipfs/bafkreifmte3p7v2wr6xgocvmkdkbrazqzvw2dvwbqj27xxjbm566nn6d4a",
+    "https://orange-chemical-monkey-267.mypinata.cloud/ipfs/bafkreid6aumxvaznq3fxpmdcnqgqva7btuguawezpsl2rfqu6xlctn6yzu",
+    "https://orange-chemical-monkey-267.mypinata.cloud/ipfs/bafkreifgxw7pb76j6ym2rvmhqfo57iqpkmkylapdxjejcbrckfywmvgdrq",
+  ];
+  const neutralImageUri = [
+    "https://orange-chemical-monkey-267.mypinata.cloud/ipfs/bafkreiewmm2zdidrabxyeqedfor4tu3utnmcultzbtx3uzmdezngjzbacm",
+    "https://orange-chemical-monkey-267.mypinata.cloud/ipfs/bafkreibpmrws6mcgbq5zf4jumjzczjbahub3f7axopqxibn5paeioqxuji",
+    "https://orange-chemical-monkey-267.mypinata.cloud/ipfs/bafkreidug7yol5rdlpzs7nzjdkdxnkcdxpqk5bxni2bauy6vxjp2wqoxge",
+  ];
+  const hungryImageUri = [
+    "https://orange-chemical-monkey-267.mypinata.cloud/ipfs/bafkreid3apinl4sygihf6j4yutstxckypf32vglozaidcpfr3jn7niphyq",
+    "https://orange-chemical-monkey-267.mypinata.cloud/ipfs/bafkreia5ao3xl5ygih527zga5zr2bkekzjb2bmbamcwhvhfycljgojl5au",
+    "https://orange-chemical-monkey-267.mypinata.cloud/ipfs/bafkreib6h43narjcdmr77eo3ajlatodlhgtqpffs2yg7d43de7tzz4cit4",
+  ];
+  const boredImageUri = [
+    "https://orange-chemical-monkey-267.mypinata.cloud/ipfs/bafkreibax2wix5dolg2ykfsskrloufpocdtgsyhxb7kemlfv2xitbgac2e",
+    "https://orange-chemical-monkey-267.mypinata.cloud/ipfs/bafkreidpxzlm5wbb3qpruycxhb6dgqit7z5c43ju3hngxafueaawpp3sjy",
+    "https://orange-chemical-monkey-267.mypinata.cloud/ipfs/bafkreifdlea3cg26o2sdesjdhkfyoddaigwa5fjpka2ldbxchhelhjfizm",
+  ];
+  const stinkyImageUri = [
+    "https://orange-chemical-monkey-267.mypinata.cloud/ipfs/bafkreifa3my5pm34xjthggfecym5a54jfvc43qjhwaktjzznkegq7crez4",
+    "https://orange-chemical-monkey-267.mypinata.cloud/ipfs/bafkreiffsatpejny5aksjmrb54i5uopyz2nxdm45jb56tikpe4tvjdjwy4",
+    "https://orange-chemical-monkey-267.mypinata.cloud/ipfs/bafkreihojbdnphmna7cy2hioa5fk6ahr7i3kkoiptfg64fs6ozggx7h3zq",
+  ];
+  const lethargicImageUri = [
+    "https://orange-chemical-monkey-267.mypinata.cloud/ipfs/bafkreigdvmsvmhl4x5y4y2l5q3ncu7rtcsadvwimb2lq6w5svsatfo73ca",
+    "https://orange-chemical-monkey-267.mypinata.cloud/ipfs/bafkreiabkptr5kxmsopkwxx45ksm7szbhg5c6zee47mjmvwro3jjdhk6dy",
+    "https://orange-chemical-monkey-267.mypinata.cloud/ipfs/bafkreicbytwr6w5gjjwj3uqqpdev3b6nedjwz4nhijwtq2dryu3yomwp6q",
+  ];
+  const deadImageUri = [
+    "https://orange-chemical-monkey-267.mypinata.cloud/ipfs/bafkreibw2562c7p7e5lgfb5ub6bz3yqumufxxtcptjzz4zra7nug7y7nku",
+    "https://orange-chemical-monkey-267.mypinata.cloud/ipfs/bafkreiagg6jxn3fa6sld2h6yx242b3v4jmddk2j5xr6fqamvpmv53mtgoa",
+    "https://orange-chemical-monkey-267.mypinata.cloud/ipfs/bafkreidpgno22vj3og6cs3tlbakyikpmpb6rj7z25ech3uekrmziz2lsva",
+  ];
 
   if (developmentChains.includes(networkName)) {
     const BASE_FEE = "250000000000000000";
